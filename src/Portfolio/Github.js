57b6f3b87667b97ea006cdb849282a9793cb7594
nicolas bertinat <nbertinatwork@gmail.com>
2024-02-26 12:32:37 -0300

    Deploy 1

--- src/Portfolio/Github.js
+++ src/Portfolio/Github.js
@@ -5,8 +5,8 @@ const Github = () =>{
     return(
         <div>
               <div className="behancePortfolio">
-          <a href="https://www.behance.net/gallery/137845083/Apk-WorldJob/"><img src="/elements/digmark.jpg" alt="Worldjob on Behance"/></a>
-          <a href="https://www.behance.net/gallery/185894919/Web-Admin-CRM/"><img src="/elements/piaf.jpg" alt="Admin CRM on Behance"/></a>
+          <a href="https://dig-mark.vercel.app//"><img src="/elements/digmark.jpg" alt="DigMark website"/></a>
+          <a href="https://piaf.vercel.app/"><img src="/elements/piaf.jpg" alt="Piaf website"/></a>
           <a href="https://www.behance.net/gallery/157076505/Apk-Claro-Video-Redesign"><img src="/elements/globalShop.jpg" alt="FranklinCovey link"/></a>
 
           </div>
