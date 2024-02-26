18eb6122538d829eb512c3e27b8d878016a922af
nicolas bertinat <nbertinatwork@gmail.com>
2024-02-26 16:41:10 -0300

    Deploy 3

--- src/Portfolio/Behance.js
+++ src/Portfolio/Behance.js
@@ -7,10 +7,10 @@ const Behance = () =>{
           <div className="behancePortfolio">
           <a href="https://www.behance.net/gallery/137845083/Apk-WorldJob/"><img src="/elements/worldjob.png" alt="Worldjob on Behance"/></a>
           <a href="https://www.behance.net/gallery/185894919/Web-Admin-CRM/"><img src="/elements/admin.png" alt="Admin CRM on Behance"/></a>
-          <a href="https://www.behance.net/gallery/157076505/Apk-Claro-Video-Redesign"><img src="/elements/claro.png" alt="FranklinCovey link"/></a>
-          <a href="https://www.behance.net/gallery/186168477/Web-Adventure-Travel"><img src="/elements/travel.png" alt="FranklinCovey link"/></a>
-          <a href="https://www.behance.net/gallery/144315741/Apk-Pet-Packages"><img src="/elements/despegar.png" alt="FranklinCovey link"/></a>
-          <a href="https://www.behance.net/gallery/185201475/Web-Coffe-Shop"><img src="/elements/coffe.png" alt="FranklinCovey link"/></a>
+          <a href="https://www.behance.net/gallery/157076505/Apk-Claro-Video-Redesign"><img src="/elements/claro.png" alt="Claro Video on Behance"/></a>
+          <a href="https://www.behance.net/gallery/186168477/Web-Adventure-Travel"><img src="/elements/travel.png" alt="Adventure Travel on Behance"/></a>
+          <a href="https://www.behance.net/gallery/144315741/Apk-Pet-Packages"><img src="/elements/despegar.png" alt="Pet packages on Behance"/></a>
+          <a href="https://www.behance.net/gallery/185201475/Web-Coffe-Shop"><img src="/elements/coffe.png" alt="Coffe Shop on Behance"/></a>
 
 
           </div>
