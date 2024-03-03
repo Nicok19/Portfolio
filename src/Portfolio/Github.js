933463f1d03c1c2abed8d00658b6df69d413c734
nicolas bertinat <nbertinatwork@gmail.com>
2024-03-03 17:23:40 -0300

    Big screen version

--- src/Portfolio/Github.js
+++ src/Portfolio/Github.js
@@ -7,7 +7,7 @@ const Github = () =>{
               <div className="behancePortfolio">
           <a href="https://dig-mark.vercel.app//"><img src="/elements/digmark.jpg" alt="DigMark website"/></a>
           <a href="https://piaf.vercel.app/"><img src="/elements/piaf.jpg" alt="Piaf website"/></a>
-          <a href="https://global-shop-git-globalshoptmp-nicok19s-projects.vercel.app/"><img src="/elements/globalShop.jpg" alt="FranklinCovey link"/></a>
+          <a href="https://global-shop-9fei.vercel.app/"><img src="/elements/globalShop.jpg" alt="FranklinCovey link"/></a>
 
           </div>
         </div>
