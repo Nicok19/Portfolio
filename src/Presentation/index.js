31e0d912c3c7dbdb446843bc17f28ea88725fcb3
nicolas bertinat <nbertinatwork@gmail.com>
2024-06-12 11:02:13 -0300

    rezising 1920px

--- src/Presentation/index.js
+++ src/Presentation/index.js
@@ -11,7 +11,7 @@ const Presentation = () =>{
             
       <div className='forCell'>
       <h3>Who i am?</h3>
-      <p>In my role as a Product Designer, I've honed my skills in user-centered design principles, prototyping, and creating intuitive interfaces that enhance user experience. Now, as I delve into Front End development, I'm excited to bridge the gap between design and implementation, bringing my creative vision to life through code.</p>
+      <p>In my role as a UX/UI Designer, I've honed my skills in user-centered design principles, prototyping, and creating intuitive interfaces that enhance user experience. Now, as I delve into Front End development, I'm excited to bridge the gap between design and implementation, bringing my creative vision to life through code.</p>
       <p></p>
       </div>
         </div>
